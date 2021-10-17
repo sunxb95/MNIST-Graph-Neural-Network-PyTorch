2af1a40f3b7888cfce2d7d0b8a1841ce6a5618dc
sunxb95 <138902381+sunxb95@users.noreply.github.com>
2021-10-17 11:14:08 +0000

    Remove Travis

--- gnn_mnist.py
+++ gnn_mnist.py
@@ -14,3 +14,5 @@ class GraphNet(nn.Module):
         super(GraphNet, self).__init__()
         self.pred_edge = pred_edge
         N = image_size ** 2 # Number of pixels in the image
+        self.fc = nn.Linear(N, 10, bias = False)
+        # Create the adjacency matrix of size (N X N)
