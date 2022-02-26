d8fb86223d419eb91eec78f6b43389d673c791af
sunxb95 <138902381+sunxb95@users.noreply.github.com>
2022-02-26 08:14:06 +0000

    Coq 8.11.0, repo released, mode clean.

--- gnn_mnist.py
+++ gnn_mnist.py
@@ -73,3 +73,5 @@ class GraphNet(nn.Module):
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
