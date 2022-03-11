10813504e44c9337b61640d19ef09899beefc57a
sunxb95 <138902381+sunxb95@users.noreply.github.com>
2022-03-11 17:13:09 +0000

    Update cards.js

--- gnn_mnist.py
+++ gnn_mnist.py
@@ -100,3 +100,4 @@ def test(args, model, device, test_loader):
     test_loss /= len(test_loader.dataset)
     print(
         '\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+            test_loss, correct, len(test_loader.dataset),
